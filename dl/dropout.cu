b7e8d058c45408f0527c54d90c3ad492bd55f5ed
KH Kim <vapnik.chervonenkis@gmail.com>
2016-02-29 00:22:56 +0900

    Add dropout

--- dl/dropout.cu
+++ dl/dropout.cu
@@ -1,4 +1,10 @@
 
+  if (curandCreateGenerator(&curand_generator_, CURAND_RNG_PSEUDO_DEFAULT)
+      != CURAND_STATUS_SUCCESS ||
+      curandSetPseudoRandomGeneratorSeed(curand_generator_, cluster_seedgen())
+      != CURAND_STATUS_SUCCESS) {
+    LOG(ERROR) << "Cannot create Curand generator. Curand won't be available.";
+  }
     unsigned int* mask =
         static_cast<unsigned int*>(rand_vec_.mutable_gpu_data());
     caffe_gpu_rng_uniform(count, mask);
