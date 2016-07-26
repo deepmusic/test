43b03b427e72f28c9d4d9ecb29000b920b3100a4
KH Kim <vapnik.chervonenkis@gmail.com>
2016-07-26 18:26:29 +0900

    Update materials

---
+++ lecture/convert_lfw_labels.py
@@ -0,0 +1,50 @@
+from numpy.random import permutation
+
+def to_img_name(name, id):
+  return 'lfw2/%s/%s_%04d.jpg' % (name, name, int(id))
+
+def get_label_pair(line):
+  tokens = line.strip().split('\t')
+  if len(tokens) == 3:
+    name = tokens[0]
+    id1 = tokens[1]
+    id2 = tokens[2]
+    return (to_img_name(name, id1), to_img_name(name, id2), 1)
+  if len(tokens) == 4:
+    name1 = tokens[0]
+    id1 = tokens[1]
+    name2 = tokens[2]
+    id2 = tokens[3]
+    return (to_img_name(name1, id1), to_img_name(name2, id2), 0)
+  return None
+
+def get_label_sets(filename, shuffled=False):
+  set1 = []
+  set2 = []
+  for line in open(filename, 'r'):
+    pair = get_label_pair(line)
+    if pair is None:
+      continue
+    img_name1 = pair[0]
+    img_name2 = pair[1]
+    sim_label = pair[2]
+    set1.append([img_name1, sim_label])
+    set2.append([img_name2, sim_label])
+  shuffled_idx = permutation(len(set1))
+  set1 = [set1[i] for i in shuffled_idx]
+  set2 = [set2[i] for i in shuffled_idx]
+  return (set1, set2)
+
+def make_lmdb_label_sets(src_file, dst1_file, dst2_file, shuffled=False):
+  set1, set2 = get_label_sets(src_file, shuffled=shuffled)
+  f = open(dst1_file, 'w')
+  for img_name, label in set1:
+    f.write('%s %d\n' % (img_name, label))
+  f.close()
+  f = open(dst2_file, 'w')
+  for img_name, label in set2:
+    f.write('%s %d\n' % (img_name, label))
+  f.close()
+
+make_lmdb_label_sets('pairsDevTrain.txt', 'train_1.txt', 'train_2.txt', shuffled=True)
+make_lmdb_label_sets('pairsDevTest.txt', 'test_1.txt', 'test_2.txt')
--- lecture/squeeze.py
+++ lecture/squeeze.py
@@ -1,28 +1,26 @@
 import caffe
 
-def param(layer_name, param_id, lr_mult=1, decay_mult=1):
+def param(param_name, param_id=0, lr_mult=1, decay_mult=1):
   param = caffe.proto.caffe_pb2.ParamSpec()
-  param.name = '{:s}/param{:d}'.format(layer_name, param_id)
+  param.name = '{:s}/param{:d}'.format(param_name, param_id)
   param.lr_mult = lr_mult
   param.decay_mult = decay_mult
   return param
 
-def param_weight_bias(layer_name, scale_mult=1, bias_term=True):
-  weight = param(layer_name, 0, lr_mult=scale_mult, decay_mult=scale_mult)
+def param_weight_bias(param_name, scale_mult=1, bias_term=True):
+  weight = param(param_name, param_id=0, lr_mult=scale_mult, decay_mult=scale_mult)
   if bias_term:
-    bias = param(layer_name, 1, lr_mult=2*scale_mult, decay_mult=0)
+    bias = param(param_name, param_id=1, lr_mult=2*scale_mult, decay_mult=0)
     return [weight, bias]
   else:
     return [weight]
 
-def empty_layer(layer_name, type_name, bottom_names, top_names, param_list=None, phase=None):
+def empty_layer(layer_name, type_name, bottom_names, top_names, phase=None):
   layer = caffe.proto.caffe_pb2.LayerParameter()
   layer.name = layer_name
   layer.type = type_name
   layer.bottom.extend(bottom_names)
   layer.top.extend(top_names)
-  if param_list is not None:
-    layer.param.extend(param_list)
   if phase is not None:
     include_param = caffe.proto.caffe_pb2.NetStateRule()
     if phase == 'TRAIN':
@@ -61,13 +59,9 @@ def dummy_data_layer(top_names=None, shapes=None, phase=None):
     layer.dummy_data_param.shape.extend([shape])
   return layer
 
-def convolution_layer(bottom_name, top_name, num_output, kernel_size, stride=1, pad=None, group=1, bias_term=True, param_layer=None, scale_mult=1, phase=None):
+def convolution_layer(bottom_name, top_name, num_output, kernel_size, stride=1, pad=None, group=1, bias_term=True, param_ref_name=None, scale_mult=1, phase=None):
   layer_name = top_name + '/conv'
-  if param_layer is None:
-    param_list = param_weight_bias(layer_name, scale_mult=scale_mult, bias_term=bias_term)
-  else:
-    param_list = param_layer.param
-  layer = empty_layer(layer_name, 'Convolution', [bottom_name], [top_name], param_list=param_list, phase=phase)
+  layer = empty_layer(layer_name, 'Convolution', [bottom_name], [top_name], phase=phase)
   layer.convolution_param.num_output = num_output
   layer.convolution_param.kernel_h = kernel_size
   layer.convolution_param.kernel_w = kernel_size
@@ -76,8 +70,13 @@ def convolution_layer(bottom_name, top_name, num_output, kernel_size, stride=1, 
   layer.convolution_param.pad_h = pad if pad is not None else (kernel_size - 1) / 2
   layer.convolution_param.pad_w = pad if pad is not None else (kernel_size - 1) / 2
   layer.convolution_param.group = group
-  layer.convolution_param.bias_term = bias_term and len(param_list) > 1
+  layer.convolution_param.bias_term = bias_term
   layer.convolution_param.weight_filler.type = 'xavier'
+  if param_ref_name is None:
+    param_name = layer_name
+  else:
+    param_name = param_ref_name + '/conv'
+  layer.param.extend(param_weight_bias(param_name, scale_mult=scale_mult, bias_term=bias_term))
   return layer
 
 def pooling_layer(bottom_name, top_name, kernel_size, stride, pool='MAX', phase=None):
@@ -107,23 +106,25 @@ def dropout_layer(bottom_name, top_name, dropout_ratio, phase=None):
   layer.dropout_param.dropout_ratio = dropout_ratio
   return layer
 
-def batch_norm_layer(bottom_name, top_name, param_layer=None, phase=None):
+def batch_norm_layer(bottom_name, top_name, param_ref_name=None, phase=None):
   layer_name = top_name + '/bn'
-  if param_layer is None:
-    param_list = [param(layer_name, i, 0, 0) for i in range(3)]
+  layer = empty_layer(layer_name, 'BatchNorm', [bottom_name], [top_name], phase=None)
+  if param_ref_name is None:
+    param_name = layer_name
   else:
-    param_list = param_layer.param
-  layer = empty_layer(layer_name, 'BatchNorm', [bottom_name], [top_name], param_list=param_list, phase=None)
+    param_name = param_ref_name + '/bn'
+  layer.param.extend([param(param_name, param_id=i, lr_mult=0, decay_mult=0) for i in range(3)])
   return layer
 
-def scale_layer(bottom_name, top_name, bias_term=True, param_layer=None, scale_mult=1, phase=None):
+def scale_layer(bottom_name, top_name, bias_term=True, param_ref_name=None, scale_mult=1, phase=None):
   layer_name = top_name + '/scale'
-  if param_layer is None:
-    param_list = param_weight_bias(layer_name, scale_mult=scale_mult, bias_term=bias_term)
+  layer = empty_layer(layer_name, 'Scale', [bottom_name], [top_name], phase=None)
+  layer.scale_param.bias_term = bias_term
+  if param_ref_name is None:
+    param_name = layer_name
   else:
-    param_list = param_layer.param
-  layer = empty_layer(layer_name, 'Scale', [bottom_name], [top_name], param_list=param_list, phase=None)
-  layer.scale_param.bias_term = bias_term and len(param_list) > 1
+    param_name = param_ref_name + '/scale'
+  layer.param.extend(param_weight_bias(param_name, scale_mult=scale_mult, bias_term=bias_term))
   return layer
 
 def relu_layer(bottom_name, top_name, phase=None):
@@ -137,11 +138,19 @@ def concat_layer(bottom_names, top_name, axis=1, phase=None):
   layer.concat_param.axis = axis
   return layer
 
-def loss_layer(bottom_name, top_name, label_name=None, phase=None):
-  layer_name = top_name + '/loss'
+def softmax_loss_layer(bottom_name, top_name, label_name=None, phase=None):
+  layer_name = top_name + '/softmax_loss'
   if label_name is None:
     label_name = 'label'
   layer = empty_layer(layer_name, 'SoftmaxWithLoss', [bottom_name, label_name], [top_name], phase=None)
+  return layer
+
+def contrastive_loss_layer(bottom1_name, bottom2_name, top_name, label_name=None, margin=1, phase=None):
+  layer_name = top_name + '/contrastive_loss'
+  if label_name is None:
+    label_name = 'label'
+  layer = empty_layer(layer_name, 'ContrastiveLoss', [bottom1_name, bottom2_name, label_name], [top_name], phase=None)
+  layer.contrastive_loss_param.margin = margin
   return layer
 
 def accuracy_layer(bottom_name, top_name, label_name=None, top_k=1, phase=None):
@@ -158,70 +167,83 @@ def append(lst, elem):
   else:
     lst.extend([elem])
 
-def conv_relu_module(bottom_name, top_name, num_output, kernel_size, stride=1, pad=None, group=1, param_layer=None, scale_mult=1):
-  if param_layer is None:
-    param_layer = [None, None]
+def conv_relu_module(bottom_name, top_name, num_output, kernel_size, stride=1, pad=None, group=1, param_ref_name=None, scale_mult=1):
   module = []
-  append(module, convolution_layer(bottom_name, top_name, num_output, kernel_size, stride=stride, pad=pad, group=group, bias_term=True, param_layer=param_layer[0], scale_mult=scale_mult))
+  append(module, convolution_layer(bottom_name, top_name, num_output, kernel_size, stride=stride, pad=pad, group=group, bias_term=True, param_ref_name=param_ref_name, scale_mult=scale_mult))
   append(module, relu_layer(top_name, top_name))
   return module
 
-def conv_scale_relu_module(bottom_name, top_name, num_output, kernel_size, stride=1, pad=None, group=1, param_layer=None, scale_mult=1):
-  if param_layer is None:
-    param_layer = [None, None, None]
+def conv_scale_relu_module(bottom_name, top_name, num_output, kernel_size, stride=1, pad=None, group=1, param_ref_name=None, scale_mult=1):
   module = []
-  append(module, convolution_layer(bottom_name, top_name, num_output, kernel_size, stride=stride, pad=pad, group=group, bias_term=False, param_layer=param_layer[0], scale_mult=scale_mult))
-  append(module, scale_layer(top_name, top_name, bias_term=True, param_layer=param_layer[1], scale_mult=scale_mult))
+  append(module, convolution_layer(bottom_name, top_name, num_output, kernel_size, stride=stride, pad=pad, group=group, bias_term=False, param_ref_name=param_ref_name, scale_mult=scale_mult))
+  append(module, scale_layer(top_name, top_name, bias_term=True, param_ref_prefix=param_ref_prefix, scale_mult=scale_mult))
   append(module, relu_layer(top_name, top_name))
   return module
 
-def conv_bn_scale_relu_module(bottom_name, top_name, num_output, kernel_size, stride=1, pad=None, group=1, param_layer=None, scale_mult=1):
-  if param_layer is None:
-    param_layer = [None, None, None, None]
+def conv_bn_scale_relu_module(bottom_name, top_name, num_output, kernel_size, stride=1, pad=None, group=1, param_ref_name=None, scale_mult=1):
   module = []
-  append(module, convolution_layer(bottom_name, top_name, num_output, kernel_size, stride=stride, pad=pad, group=group, bias_term=False, param_layer=param_layer[0], scale_mult=scale_mult))
-  append(module, batch_norm_layer(top_name, top_name, param_layer=None))
-  append(module, scale_layer(top_name, top_name, bias_term=True, param_layer=param_layer[2], scale_mult=scale_mult))
+  append(module, convolution_layer(bottom_name, top_name, num_output, kernel_size, stride=stride, pad=pad, group=group, bias_term=False, param_ref_name=param_ref_name, scale_mult=scale_mult))
+  append(module, batch_norm_layer(top_name, top_name, param_ref_name=param_ref_name))
+  append(module, scale_layer(top_name, top_name, bias_term=True, param_ref_name=param_ref_name, scale_mult=scale_mult))
   append(module, relu_layer(top_name, top_name))
   return module
 
-def squeeze_module(conv_module, bottom_name, top_name, num_squeeze, num_expand, param_layer=None, scale_mult=1):
-  if param_layer is None:
-    param_layer = [None, None, None, None]
+def squeeze_module(conv_module, bottom_name, top_name, num_squeeze, num_expand, param_ref_name=None, scale_mult=1):
   squeeze_name = top_name + '/squeeze1x1'
   expand1x1_name = top_name + '/expand1x1'
   expand3x3_name = top_name + '/expand3x3'
+
+  if param_ref_name is None:
+    param_ref_name = top_name
+  squeeze_ref = param_ref_name + '/squeeze1x1'
+  expand1x1_ref = param_ref_name + '/expand1x1'
+  expand3x3_ref = param_ref_name + '/expand3x3'
+
   module = []
-  append(module, conv_module(bottom_name, squeeze_name, num_squeeze, 1, param_layer=param_layer[0], scale_mult=scale_mult))
-  append(module, conv_module(squeeze_name, expand1x1_name, num_expand, 1, param_layer=param_layer[1], scale_mult=scale_mult))
-  append(module, conv_module(squeeze_name, expand3x3_name, num_expand, 3, param_layer=param_layer[2], scale_mult=scale_mult))
+  append(module, conv_module(bottom_name, squeeze_name, num_squeeze, 1, param_ref_name=squeeze_ref, scale_mult=scale_mult))
+  append(module, conv_module(squeeze_name, expand1x1_name, num_expand, 1, param_ref_name=expand1x1_ref, scale_mult=scale_mult))
+  append(module, conv_module(squeeze_name, expand3x3_name, num_expand, 3, param_ref_name=expand3x3_ref, scale_mult=scale_mult))
   append(module, concat_layer([expand1x1_name, expand3x3_name], top_name))
   return module
 
-def squeeze_net():
+def squeeze_net(net=None, name="", param_ref_name="", scale_mult=1):
+  if net is None:
+    net = caffe.proto.caffe_pb2.NetParameter()
+  conv_module = conv_bn_scale_relu_module
+  append(net.layer, conv_module('data', name+'conv1', 64, 3, stride=2, pad=0, param_ref_name=param_ref_name+'conv1', scale_mult=scale_mult))
+  append(net.layer, pooling_layer(name+'conv1', name+'pool1', 3, 2, pool='MAX'))
+  append(net.layer, squeeze_module(conv_module, name+'pool1', name+'fire2', 16, 64, param_ref_name=param_ref_name+'fire2', scale_mult=scale_mult))
+  append(net.layer, squeeze_module(conv_module, name+'fire2', name+'fire3', 16, 64, param_ref_name=param_ref_name+'fire3', scale_mult=scale_mult))
+  append(net.layer, pooling_layer(name+'fire3', name+'pool3', 3, 2, pool='MAX'))
+  append(net.layer, squeeze_module(conv_module, name+'pool3', name+'fire4', 32, 128, param_ref_name=param_ref_name+'fire4', scale_mult=scale_mult))
+  append(net.layer, squeeze_module(conv_module, name+'fire4', name+'fire5', 32, 128, param_ref_name=param_ref_name+'fire5', scale_mult=scale_mult))
+  append(net.layer, pooling_layer(name+'fire5', name+'pool5', 3, 2, pool='MAX'))
+  append(net.layer, squeeze_module(conv_module, name+'pool5', name+'fire6', 48, 192, param_ref_name=param_ref_name+'fire6', scale_mult=scale_mult))
+  append(net.layer, squeeze_module(conv_module, name+'fire6', name+'fire7', 48, 192, param_ref_name=param_ref_name+'fire7', scale_mult=scale_mult))
+  append(net.layer, squeeze_module(conv_module, name+'fire7', name+'fire8', 64, 256, param_ref_name=param_ref_name+'fire8', scale_mult=scale_mult))
+  append(net.layer, squeeze_module(conv_module, name+'fire8', name+'fire9', 64, 256, param_ref_name=param_ref_name+'fire9', scale_mult=scale_mult))
+  append(net.layer, dropout_layer(name+'fire9', name+'fire9', 0.5))
+  append(net.layer, conv_module(name+'fire9', name+'conv10', 1000, 1, param_ref_name=param_ref_name+'conv10', scale_mult=scale_mult))
+  append(net.layer, global_pooling_layer(name+'conv10', name+'out', pool='AVE'))
+  return net
+
+def classifier():
   net = caffe.proto.caffe_pb2.NetParameter()
-  conv_module = conv_relu_module
-  #append(net.layer, dummy_data_layer())
   append(net.layer, data_layer('data/imagenet/ilsvrc12_train_lmdb', batch_size=64, phase='TRAIN'))
   append(net.layer, data_layer('data/imagenet/ilsvrc12_val_lmdb', phase='TEST'))
-  append(net.layer, conv_module('data', 'conv1', 64, 3, stride=2, pad=0))
-  append(net.layer, pooling_layer('conv1', 'pool1', 3, 2, pool='MAX'))
-  append(net.layer, squeeze_module(conv_module, 'pool1', 'fire2', 16, 64))
-  append(net.layer, squeeze_module(conv_module, 'fire2', 'fire3', 16, 64))
-  append(net.layer, pooling_layer('fire3', 'pool3', 3, 2, pool='MAX'))
-  append(net.layer, squeeze_module(conv_module, 'pool3', 'fire4', 32, 128))
-  append(net.layer, squeeze_module(conv_module, 'fire4', 'fire5', 32, 128))
-  append(net.layer, pooling_layer('fire5', 'pool5', 3, 2, pool='MAX'))
-  append(net.layer, squeeze_module(conv_module, 'pool5', 'fire6', 48, 192))
-  append(net.layer, squeeze_module(conv_module, 'fire6', 'fire7', 48, 192))
-  append(net.layer, squeeze_module(conv_module, 'fire7', 'fire8', 64, 256))
-  append(net.layer, squeeze_module(conv_module, 'fire8', 'fire9', 64, 256))
-  append(net.layer, dropout_layer('fire9', 'fire9', 0.5))
-  append(net.layer, conv_module('fire9', 'conv10', 1000, 1))
-  append(net.layer, global_pooling_layer('conv10', 'pool10', pool='AVE'))
-  append(net.layer, loss_layer('pool10', 'loss'))
-  append(net.layer, accuracy_layer('pool10', 'accuracy', phase='TEST'))
-  append(net.layer, accuracy_layer('pool10', 'accuracy_top5', top_k=5, phase='TEST'))
+  squeeze_net(net=net)
+  append(net.layer, accuracy_layer('out', 'accuracy', phase='TEST'))
+  append(net.layer, accuracy_layer('out', 'accuracy_top5', top_k=5, phase='TEST'))
+  return net
+
+def siamese_net():
+  #append(net.layer, dummy_data_layer())
+  net = caffe.proto.caffe_pb2.NetParameter()
+  append(net.layer, data_layer('data/imagenet/ilsvrc12_train_lmdb', batch_size=64, phase='TRAIN'))
+  append(net.layer, data_layer('data/imagenet/ilsvrc12_val_lmdb', phase='TEST'))
+  squeeze_net(net=net, name="")
+  squeeze_net(net=net, name="B/", param_ref_name="")
+  append(net.layer, contrastive_loss_layer('out', 'B/out', 'loss'))
   return net
 
 def net_to_file(net, pt_filename):
@@ -232,8 +254,10 @@ def net_to_file(net, pt_filename):
 def copy_squeeze_net(src, dst):
   for key in dst._layer_names:
     if key.endswith('/conv'):
-      print key
       src_key = key[:-5]
+      if not src.params.has_key(src_key):
+        continue
+      print key
       weight = src.params[src_key][0].data
       bias = src.params[src_key][1].data
       scale_key = key[:-5] + '/scale'
@@ -260,6 +284,6 @@ def save_squeeze_net(pt_src, model_src, pt_dst, model_dst):
   copy_squeeze_net(src, dst)
   dst.save(model_dst)
 
-net = squeeze_net()
+net = classifier()
 net_to_file(net, 'myproto.pt')
 save_squeeze_net('SqueezeNet/SqueezeNet_v1.1/train_val.prototxt', 'SqueezeNet/SqueezeNet_v1.1/squeezenet_v1.1.caffemodel', 'myproto.pt', 'mymodel.cm')
